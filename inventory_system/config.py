487491ade8cc7193a7ee0f234c230d2d7ef169b3
Zheng Taing <78318818+ZhengTaing@users.noreply.github.com>
2024-10-30 16:35:50 -0700

    updated config.py to address security measures and auth.py's login function to clear any pre-existing sessionid

--- inventory_system/config.py
+++ inventory_system/config.py
@@ -3,7 +3,16 @@ from datetime import timedelta
 
 
 class Config:
+    
+    #random key generator
     SECRET_KEY = os.urandom(24)
+    
+    #ensures cookies are only sent over HTTP
+    SESSION_COOKIE_SECURE = True
+    
+    #prevents javascript from accessing cookies, prevent cross-site scripting (injection of malicious scripts)
+    SESSION_COOKIE_HTTPONLY = True
+    
     SQLALCHEMY_DATABASE_URI = 'sqlite:///inventory.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
