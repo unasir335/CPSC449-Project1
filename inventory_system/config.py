bbb245d2bfbea8934b9a35ff48ae154a3a78deeb
Zheng Taing <78318818+ZhengTaing@users.noreply.github.com>
2024-11-30 21:06:39 -0800

    connected the GET, GET(specific), POST methods to both databases

--- inventory_system/config.py
+++ inventory_system/config.py
@@ -7,16 +7,7 @@ class Config:
     #random key generator
     SECRET_KEY = os.urandom(24)
     
-<<<<<<< HEAD
-    #ensures cookies are only sent over HTTP
-    SESSION_COOKIE_SECURE = True
-    
-    #prevents javascript from accessing cookies, prevent cross-site scripting (injection of malicious scripts)
-    SESSION_COOKIE_HTTPONLY = True
-    
-=======
     #mysql config
->>>>>>> 4582e4c (connected the GET, GET(specific), POST methods to both databases)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///inventory.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
